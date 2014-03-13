fb346310aad54c8a961c36c62c24cf871a17324c
Michael Catanzaro <michael.catanzaro@mst.edu>
2014-03-13 16:31:35 -0500

    Add license: GPLv3+

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
 # -*- mode: python; tab-width: 4; indent-tabs-mode: nil -*-
+#
+# Copyright © 2014 Michael Catanzaro
+# Copyright © 2014 Levi Mallot
+#
+# This file is part of RKP Attack.
+#
+# RKP Attack is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RKP Attack is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RKP Attack.  If not, see <http://www.gnu.org/licenses/>.
 
 import networkx as nx
 import matplotlib.pyplot as plt
